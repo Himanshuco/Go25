811a4623393919fd2310e6187de6a6d3e36834a1
Himanshuco <shimanshu0405@gmail.com>
2025-04-03 23:08:28 +0530

    Varibale Basics (1) program

---
+++ CH 1 Variables/Basic.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a int //A variable named "a" have a integer type
+
+var b int = 2 ////A variable named "b" have a integer type and value 2
+
+var c = 3
+
+func main() {
+	a = 1
+	fmt.Println("a=", a)
+	fmt.Println("b=", b)
+	fmt.Println("c=", c)
+}
